9c15785617a290477b81938903b722bce5975b5d
Namito Hirezaki <7310wave@gmail.com>
2024-11-29 22:49:17 +0900

    update

--- api/init/12_purchase.sql
+++ api/init/12_purchase.sql
@@ -5,7 +5,7 @@ CREATE TABLE purchase(
 	userId VARCHAR(36) NOT NULL,
 	listingId BIGINT NOT NULL,
 	created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	status ENUM('listing', 'completed', 'cancelled','purchased') NOT NULL DEFAULT 'purchased',
+	status ENUM('注文確定', '配送中', '出荷完了','キャンセル') NOT NULL DEFAULT '注文確定',
 	FOREIGN KEY (userId) REFERENCES users(userId),
 	FOREIGN KEY (listingId) REFERENCES listing(listingId)
 );
