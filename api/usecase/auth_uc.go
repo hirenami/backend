1be3ab6eaae5b7cae165a99e2acef501fd6c2ab4
Namito Hirezaki <7310wave@gmail.com>
2024-12-03 18:23:16 +0900

    update

--- api/usecase/auth_uc.go
+++ api/usecase/auth_uc.go
@@ -10,16 +10,22 @@ import (
 
 
 func (u *Usecase) Initializing() *firebase.App {
+    credsFile := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    ctx := context.Background()
 
-	credsFile := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    var app *firebase.App
+    var err error
+
     if credsFile == "" {
-        log.Fatalf("GOOGLE_APPLICATION_CREDENTIALS is not set")
+        // デフォルトの認証情報を使用 (GCP環境向け)
+        app, err = firebase.NewApp(ctx, nil)
+    } else {
+        // ローカル開発環境向け
+        app, err = firebase.NewApp(ctx, nil, option.WithCredentialsFile(credsFile))
     }
 
-    ctx := context.Background()
-    app, err := firebase.NewApp(ctx, nil, option.WithCredentialsFile(credsFile))
     if err != nil {
-        log.Fatalf("error initializing app: %v\n", err)
+        log.Fatalf("Error initializing Firebase App: %v\n", err)
     }
 
     return app
