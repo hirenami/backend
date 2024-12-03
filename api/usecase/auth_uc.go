64caca233db13013cbbda210f54c54bb848fab66
Namito Hirezaki <7310wave@gmail.com>
2024-12-03 19:45:37 +0900

    update

--- api/usecase/auth_uc.go
+++ api/usecase/auth_uc.go
@@ -6,28 +6,63 @@ import (
 	"google.golang.org/api/option"
 	"log"
 	"os"
+	"strings"
+	"encoding/json"
 )
 
 
 func (u *Usecase) Initializing() *firebase.App {
-    credsFile := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
-    ctx := context.Background()
 
-    var app *firebase.App
-    var err error
+	typeEnv := os.Getenv("TYPE")
+	projectID := os.Getenv("PROJECT_ID")
+	privateKeyID := os.Getenv("PRIVATE_KEY_ID")
+	privateKey := os.Getenv("PRIVATE_KEY")
+	clientEmail := os.Getenv("CLIENT_EMAIL")
+	clientID := os.Getenv("CLIENT_ID")
+	authURI := os.Getenv("AUTH_URI")
+	tokenURI := os.Getenv("TOKEN_URI")
+	authProviderCertURL := os.Getenv("AUTH_PROVIDER_X509_CERT_URL")
+	clientCertURL := os.Getenv("CLIENT_X509_CERT_URL")
+	universe_domain := os.Getenv("UNIVERSE_DOMAIN")
 
-    if credsFile == "" {
-        // デフォルトの認証情報を使用 (GCP環境向け)
-        app, err = firebase.NewApp(ctx, nil)
-    } else {
-        // ローカル開発環境向け
-        app, err = firebase.NewApp(ctx, nil, option.WithCredentialsFile(credsFile))
-    }
+	// 必要な情報がすべて環境変数から取得できたかを確認
+	if typeEnv == "" || projectID == "" || privateKeyID == "" || privateKey == "" || clientEmail == ""  || clientID == "" || authURI == "" || tokenURI == "" || authProviderCertURL == "" || clientCertURL == "" || universe_domain == "" {
+		log.Println("Required environment variables are missing.")
+	}
 
-    if err != nil {
-        log.Fatalf("Error initializing Firebase App: %v\n", err)
-    }
+	// 秘密鍵を実際の改行文字に変換
+	privateKey = strings.ReplaceAll(privateKey, "\\n", "\n")
 
-    return app
+	// JSON構造体を作成
+	serviceAccount := map[string]interface{}{
+		"type":                      typeEnv,
+		"project_id":                projectID,
+		"private_key_id":            privateKeyID,
+		"private_key":               privateKey,
+		"client_email":              clientEmail,
+		"client_id":                 clientID,
+		"auth_uri":                  authURI,
+		"token_uri":                 tokenURI,
+		"auth_provider_x509_cert_url": authProviderCertURL,
+		"client_x509_cert_url":      clientCertURL,
+		"universe_domain":           universe_domain,
+	}
+
+	// JSONにエンコード
+	serviceAccountJSON, err := json.Marshal(serviceAccount)
+	if err != nil {
+		log.Printf("Failed to marshal service account JSON: %v", err)
+		log.Println("error")
+	}
+
+	ctx := context.Background()
+	opt := option.WithCredentialsJSON(serviceAccountJSON)
+	
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		log.Printf("error initializing app: %v\n", err)
+		log.Println("error2")
+	}
+	return app
 }
 
